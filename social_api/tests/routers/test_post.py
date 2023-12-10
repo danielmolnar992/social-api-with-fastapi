fd5cfd375b85b4802b905a465d0cec19c56cc4e6
Daniel Molnar <daniel.molnar@worldofbook.com>
2023-12-10 17:18:54 +0000

    Remove whitespace

--- social_api/tests/routers/test_post.py
+++ social_api/tests/routers/test_post.py
@@ -232,7 +232,7 @@ async def test_get_post_with_comments(
     response = await async_client.get(f'/post/{created_post['id']}')
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {
-        'post': {**created_post, 'likes': 0 },
+        'post': {**created_post, 'likes': 0},
         'comments': [created_comment],
     }
 
